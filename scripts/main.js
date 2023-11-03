0c886f3f16e4b98e04e76a1e0f939241c425b0b0
Mohammed Mowla <m.mowla03151@gmail.com>
2023-11-03 15:48:21 -0400

    Adding orbital controls and gltf

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,69 @@
 import * as THREE from 'three';
-//import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Setting the Scene, Camera, and Renderer
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio)
-renderer.setSize( window.innerWidth/1.2, window.innerHeight/1.2 );
+renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-//GLTF Loader
-//const loader = new GLTFLoader();
+camera.position.setZ(30); //Prevents the camera and the torus spawning in one another, moves the camera up
 
+
+//GLTF Loader
+const loader = new GLTFLoader();
+loader.load(
+	//resource url
+	'/Models/delauney_reverse.glb',
+	//called when the resource is loaded
+	function( gltf ){
+
+		scene.add( gltf.scene );
+
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
 //Addding a torus
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 ); //Defines Points, Vertices, Faces. etc
-const material = new THREE.MeshBasicMaterial( { color: 0xFF6347} ); //Material for the torus
+const material = new THREE.MeshStandardMaterial( { color: 0xFF6347} ); //Material for the torus
 const donut = new THREE.Mesh( geometry, material ); //Actual mesh object that connects the geometry and gives a material look to it
 scene.add(donut); //Adding the torus to the scene
 
-camera.position.z = 30; //Prevents the camera and the torus spawning in one another, moves the camera up
+//Adding a light
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(0, 0, 0)
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 10.0000);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+
+
 
 // Render/Animate loop (Called normally 60 times per second)
 function animate() {
@@ -28,6 +72,8 @@ function animate() {
 	// Rotate the torus
 	donut.rotation.x += 0.01;
 	donut.rotation.y += 0.01;
+
+	controls.update();
 	renderer.render( scene, camera );
 }
 
