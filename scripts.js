2a86b3545bb648a1f6a0fe0ab3152c6d0857b4cb
Mohammed Mowla <m.mowla03151@gmail.com>
2023-11-05 15:22:00 -0500

    Trying to fix mobile issue using npm

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 // INSTANTIATIONS
 const scene = new THREE.Scene(0xffffff);                                                                   // SCENE
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);        // CAMERA
-const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});                                  // RENDERER
+const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});                              // RENDERER
 const gltfloader = new GLTFLoader();                                                                       // GLTF Loader
 //const textLoader = new TextLoader();
 
