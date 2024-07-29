7e843d924327818b7de2a35316c7a7529438f45a
Collyz <m.mowla03151@gmail.com>
2024-07-28 21:03:02 -0400

    Adding resume to assets and drive

--- dev/src/lib/Resume.svelte
+++ dev/src/lib/Resume.svelte
@@ -7,8 +7,9 @@
 <p class="flex justify-center tracking-tight text-4xl py-8">
     Resume
 </p>
-<main class="flex flex-col items-center justify-center min-h-full min-w-full">
-    <iframe class="flex-grow min-w-full min-h-screen" title="Mohammd Mowla's Resume" src="./src/assets/resume.pdf"></iframe>
+<main class="items-center justify-center min-h-full min-w-full">
+    <iframe title="Mohammed Mowla's Resume" class="min-w-full min-h-screen" src="https://drive.google.com/file/d/1CjLGNcibvDkRj-39tdj3JqIa78G15Dc4/preview" width="640" height="480" allow="autoplay"></iframe>
+    <!-- <iframe  title="Mohammd Mowla's Resume" src="./src/assets/resume.pdf" allow="fullscreen"></iframe> -->
     <!--  <iframe class="flex-grow w-full h-screen" title="Mohammed Mowla's Resume" 
     src="https://drive.google.com/file/d/1ZIzCmbyars0g0Mb-Fw8J25N01T-txlOR/preview" allow="autoplay"></iframe>  -->    
     <!-- <iframe src="../../resume/test.html" width="100%" height="100%" title="Mohammed Mowla's Resume"></iframe> -->
