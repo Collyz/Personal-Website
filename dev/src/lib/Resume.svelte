0b8b77208b4f60d94f31037c8ac2f193b6771a2b
Collyz <m.mowla03151@gmail.com>
2024-07-29 23:33:37 -0400

    New Resume, fixed tab svg, fixed intro sentence

--- dev/src/lib/Resume.svelte
+++ dev/src/lib/Resume.svelte
@@ -8,7 +8,7 @@
     Resume
 </p>
 <main class="items-center justify-center min-h-full min-w-full">
-    <iframe title="Mohammed Mowla's Resume" class="min-w-full min-h-screen" src="https://drive.google.com/file/d/1CjLGNcibvDkRj-39tdj3JqIa78G15Dc4/preview" width="640" height="480" allow="autoplay"></iframe>
+    <iframe title="Mohammed Mowla's Resume" class="min-w-full min-h-screen" src="https://drive.google.com/file/d/1CjLGNcibvDkRj-39tdj3JqIa78G15Dc4/preview" allow="autoplay"></iframe>
     <!-- <iframe  title="Mohammd Mowla's Resume" src="./src/assets/resume.pdf" allow="fullscreen"></iframe> -->
     <!--  <iframe class="flex-grow w-full h-screen" title="Mohammed Mowla's Resume" 
     src="https://drive.google.com/file/d/1ZIzCmbyars0g0Mb-Fw8J25N01T-txlOR/preview" allow="autoplay"></iframe>  -->    
