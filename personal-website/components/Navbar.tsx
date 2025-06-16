3df0ec9c50b9a99c3391fbced4f4332e3a3da7ea
Collyz <m.mowla03151@gmail.com>
2025-06-16 17:48:01 -0400

    Navbar 'about' -> 'experience'

--- personal-website/components/Navbar.tsx
+++ personal-website/components/Navbar.tsx
@@ -5,7 +5,7 @@ export default function Navbar() {
     <nav className="flex justify-center items-center p-4 py-24">
       <ul className="flex gap-8 text-lg">
         <li><a href="/" className="hover:underline hover:bg-sky-700">Home</a></li>
-        <li><a href="/about" className="hover:underline hover:bg-sky-700">About</a></li>
+        <li><a href="/experiences" className="hover:underline hover:bg-sky-700">Experience</a></li>
         <li><a href="/projects" className="hover:underline">Projects</a></li>
         <li><a href="/contact" className="hover:underline">Contact</a></li>
       </ul>
