8b29cd139b318800edfea0a14774908c2293a12a
Collyz <m.mowla03151@gmail.com>
2025-06-17 20:39:28 -0400

    Moved card component

--- personal-website/app/page.tsx
+++ personal-website/app/page.tsx
@@ -2,26 +2,11 @@
 import React, { ReactNode} from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import { AnimatedTorus, OrbitTori} from '../components/ThreeVisuals';
-import Navbar from '../components/Navbar';
+import { OrbitTori } from '../components/ThreeVisuals';
+import { Navbar } from '../components/Navbar';
 
 
-type CardProps = {
-  title: string;
-  description: string;
-  href: string;
-  children?: ReactNode; // For image or canvas
-};
 
-function Card({ title, description, href }: { title: string; description: string; href: string }) {
-  return (
-    <a href={href} className="card">
-      <h2 className="text-2xl font-bold">{title} </h2>
-      <p className="text-lg">{description}</p>  
-    </a>
-
-  );
-}
 
 export default function HomeContent() {
   const oceanRef = React.useRef<any>(null);
