5bee31c4fe1f20ff90a889146bd80a5c210a19e8
Collyz <m.mowla03151@gmail.com>
2025-06-17 20:41:07 -0400

    Navbar animated fade in from top

--- personal-website/components/Navbar.tsx
+++ personal-website/components/Navbar.tsx
@@ -1,14 +1,20 @@
 'use client'
+import { motion } from 'framer-motion';
 
-export default function Navbar() {
-  return ( 
-    <nav className="flex justify-center items-center p-4 py-24">
-      <ul className="flex gap-8 text-lg">
-        <li><a href="/" className="hover:underline hover:bg-sky-700">Home</a></li>
-        <li><a href="/experiences" className="hover:underline hover:bg-sky-700">Experience</a></li>
+export function Navbar() {
+  return (
+    <motion.nav
+      initial={{ y: -50, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ delay: .7, duration: 1.8, ease: "easeOut" }}
+      className="flex justify-center items-center p-4 py-24"
+    >
+      <ul className="flex gap-6 text-lg">
+        <li><a href="/" className="hover:underline">Home</a></li>
+        <li><a href="/experiences" className="hover:underline">Experience</a></li>
         <li><a href="/projects" className="hover:underline">Projects</a></li>
-        <li><a href="/contact" className="hover:underline">Contact</a></li>
+        <li><a href="/contact" className="hover:underline">Resume</a></li>
       </ul>
-    </nav>
+    </motion.nav>
   );
 }
