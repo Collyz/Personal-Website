2dc1ee57eb74b7c426511574ea21ac7ed938f2c1
Collyz <m.mowla03151@gmail.com>
2025-06-23 10:34:13 -0400

    Orbiting Tori 3 sec opacity fade in on page load

--- personal-website/components/ThreeVisuals.tsx
+++ personal-website/components/ThreeVisuals.tsx
@@ -20,9 +20,13 @@ export function AnimatedTorus({
   startAngle?: number;
 }) {
   const meshRef = useRef<THREE.Mesh>(null);
+  const materialRef = useRef<THREE.MeshStandardMaterial>(null);
+  const startTimeRef = useRef<number | null>(null);
 
 // Updating the torus
-useFrame(() => {
+useFrame(( { clock }) => {
+
+    // Positions the torus in a circular orbit and applies local rotation on all axes
     if (meshRef.current) {
       const angle = angleRef.current;
       const radius = radiusRef.current;
@@ -33,12 +37,28 @@ useFrame(() => {
       meshRef.current.rotation.z += .005;
       meshRef.current.rotation.x += .01;
     }
+
+    if (startTimeRef.current === null) {
+      startTimeRef.current = clock.getElapsedTime()
+    }
+    const elapsedTime = clock.getElapsedTime() - startTimeRef.current;
+
+    // Controls opacity with a delay
+    if (materialRef.current && elapsedTime > 3) {
+      if (materialRef.current) {
+        if (materialRef.current.opacity != 1) {
+          materialRef.current.opacity += 0.005
+        }
+    }
+    }
+
+    
   });
 
   return(
     <mesh ref={meshRef} scale={scale} rotation={[0, startAngle, 0]}>
       <torusGeometry args={[7.5, 1.75, 16, 100]}/>
-      <meshStandardMaterial color={color} wireframe={false}/>
+      <meshStandardMaterial ref={materialRef} color={color} wireframe={false} transparent opacity={0}/>
     </mesh>
   )
 }
@@ -67,6 +87,9 @@ export function OrbitTori() {
     if (groupRef.current) {
       groupRef.current.rotateZ(0.005);
     }
+
+    groupRef.current
+
 
   });
 
