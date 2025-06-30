6d3c5a7381b3ab968a5e13e09b8ec0c114a70b8d
Collyz <m.mowla03151@gmail.com>
2025-06-30 12:06:40 -0400

    Fixed main page imports, rm Cards.tsx

--- personal-website/app/projects/page.tsx
+++ personal-website/app/projects/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { Navbar } from '../../components/Navbar';
+import { Navbar } from '@/components/Navbar';
 import FadeInText from '@/components/FadeInText';
+import { ProjectCard } from '@/components/ProjectCard';
+import CascadingFadeIn from '@/components/CascadingFadeIn';
 
 export default function ProjectContent() {
     return (
@@ -9,9 +11,75 @@ export default function ProjectContent() {
             <Navbar />
             <main className="flex flex-col items-center">
                 <FadeInText text="Projects" className='py-16' additative={true}/>
-                
+                <CascadingFadeIn 
+                    components={[
+                        <ProjectCard 
+                        title={'DCGAN Animal Image Generation'} 
+                        date={'April - May 2024'} 
+                        description={''} 
+                        href={''} 
+                        show_line={true} 
+                        status={false}
+                        children={null}
+                        />,
+                        <ProjectCard 
+                        title={'Lake Fred Research Project'} 
+                        date={'January - November 2023'} 
+                        description={''} 
+                        href={'https://github.com/Collyz/Lake-Fred'} 
+                        show_line={true} 
+                        status={true}
+                        children={null}
+                        />,
+                        <ProjectCard 
+                        title={'Java Dynamic Website'} 
+                        date={'May - June 2023'} 
+                        description={''} 
+                        href={''} 
+                        show_line={true} 
+                        status={true}
+                        children={null}
+                        />,
+                        <ProjectCard 
+                        title={'Syntax Type Website'} 
+                        date={'March 2023 - April 2023'} 
+                        description={''} 
+                        href={''} 
+                        show_line={true} 
+                        status={true}
+                        children={null}
+                        />,
+                        <ProjectCard 
+                        title={'Traveling Salesperson Problem (TSP)'} 
+                        date={'April 2023'} 
+                        description={''} 
+                        href={''} 
+                        show_line={true} 
+                        status={true}
+                        children={null}
+                        />,
+                        <ProjectCard 
+                        title={'Desktop Inventory Application'} 
+                        date={'September - December 2022'} 
+                        description={''} 
+                        href={''} 
+                        show_line={true} 
+                        status={true}
+                        children={null}
+                        />,<ProjectCard 
+                        title={'Java Statistical Library'} 
+                        date={'January 2022 - May 2022'} 
+                        description={''} 
+                        href={''} 
+                        show_line={true} 
+                        status={true}
+                        children={null}
+                        />
+                        
+                    ]} 
+                    />
             </main>
-
+            <footer className='py-12'></footer>
         </div>
     );
 }
