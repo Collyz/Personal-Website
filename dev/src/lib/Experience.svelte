d11d6cfe323b584b3e6b5caf40ece6a532e68c47
Collyz <m.mowla03151@gmail.com>
2025-07-01 22:10:06 -0400

    Completed deleted svelte website files

--- dev/src/lib/Experience.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import Navbar from './Navbar.svelte';
-</script>
-  
-<Navbar />
-
-<div class="flex flex-col items-center">
-    <h1 class="tracking-tight text-4xl py-8">Experience</h1>
-</div>
--- dev/src/lib/contact.js
+++
@@ -1,7 +0,0 @@
-import Contact from './Contact.svelte';
-
-const contact = new Contact({
-    target: document.getElementById('contact'),
-  });
-
-export default contact;
