c6ff5b835a81424b8917bc9cb55fca77dfbe54bf
Collyz <m.mowla03151@gmail.com>
2025-07-01 22:06:00 -0400

    Added draft resume v1 for now

--- personal-website/app/resume/page.tsx
+++ personal-website/app/resume/page.tsx
@@ -9,7 +9,7 @@ export default function ResumeContent() {
             <Navbar />
             <main className="flex flex-col items-center">
                 <FadeInText text="Resume" className='pt-16' additative={true}/>
-                
+                <iframe title="Mohammed Mowla's Resume" className="min-w-full min-h-screen md:px-24 md:py-16 py-12" src="https://drive.google.com/file/d/1LRT8oyhgvS-UE3zIvmz-OCEe5cCtdu9D/preview" allow="autoplay"></iframe>
             </main>
 
         </div>
